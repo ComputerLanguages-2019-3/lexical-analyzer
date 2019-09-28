5ef578cabecc7cb28698b8ac3a51fdaf1e068ad7
JhonatanSV <jsandovalv@unal.edu.co>
2019-09-28 00:34:36 -0500

    :art: :sparkles: identify indentifiers tokens

--- settings.py
+++ settings.py
@@ -2,7 +2,22 @@ FILE_NAME = 'test.txt'
 
 KEY_WORD_TYPE = 'keyword'
 IDENTIFIER = 'id'
+NUMBER = {
+    'REAL': 'real_num',
+    'INT': 'int_num'
+}
 OPERATOR = {
-    'ADD': 'opadd',
-    'MUL': 'opmul'
+    'INCREASE' : 'inc_op',
+    'ADD' : 'add_op',
+    'DECREASE' : 'dec_op',
+    'SUB' : 'sub_op',
+    'MUL' : 'mul_op',
+    'DIV' : 'div_op',
+    'ASSIGN' : 'asgn_op',
+    'DEFINE' : 'def_op',
+    'GREATER' : 'gre_op',
+    'LOWER' : 'low_op',
+    'MOD' : 'mod_op',
+    'PAR_LEFT' : 'par_left',
+    'PAR_RIGHT' : 'par_right'
 }
