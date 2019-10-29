a65b6ae5f5b7032e4c26156781b427e1a1096253
chris <camantillad@unal.edu.co>
2019-10-29 13:31:20 -0500

    :bug: Solved

--- token_lex.py
+++ token_lex.py
@@ -4,25 +4,12 @@ from settings import FILE_NAME, IDENTIFIER, KEY_WORD_TYPE, OPERATOR, NUMBER,STRI
 class Token(object):
     def __str__(self):
         token_class = None
-        if self.type == KEY_WORD_TYPE:
-            token_class = '<{},{},{}>'.format(
-                self.lexeme,
-                self.row,
-                self.column
-            )
-        elif self.type[-2:] == 'op':
-            token_class = '<{},{},{}>'.format(
-                self.type,
-                self.row,
-                self.column
-            )
-        elif self.type == STRING or self.type == IDENTIFIER or self.type[-3:] == 'num':
-            token_class = '<{},{},{},{}>'.format(
-                self.type,
-                self.lexeme,
-                self.row,
-                self.column
-            )
+        token_class = '<{},{},{},{}>'.format(
+            self.type,
+            self.lexeme,
+            self.row,
+            self.column
+        )
         return token_class
 
     def __init__(self, column, row, type, lexeme):
